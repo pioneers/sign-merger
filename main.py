b25d4cd72df820e22821559a3beae98c8f011bc0
scottnuma <scott.numamoto@gmail.com>
2019-03-06 20:56:49 -0800

    Add min files for Dokku deployment

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Sign Merger in Progress'
+
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
