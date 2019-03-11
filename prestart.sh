258ca3901335b2699314a18eaccc53aaae0b336c
baby-bell <baby-bell@users.noreply.github.com>
2019-03-10 22:42:37 -0700

    Try again

--- prestart.sh
+++ prestart.sh
@@ -1,6 +1,6 @@
 
 if [ $PORT ] ; then
-    sed -si "s/listen [0-9]*;/listen $PORT/" /etc/nginx/nginx.conf
+    sed -si "s/listen [0-9]*;/listen $PORT/" /etc/nginx/conf.d/nginx.conf
 fi
 
-cat /etc/nginx/nginx.conf
+cat /etc/nginx/conf.d/nginx.conf
